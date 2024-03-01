0774af1a03bc71504bc6bdbff5cb9382cadddaf5
Jatin Verma <jatin.verma1982001@gmail.com>
2024-03-01 20:08:33 +0530

    resolving github pages-issue

---
+++ src/components/Push.jsx
@@ -0,0 +1,132 @@
+/*const App = () => {
+    const boards = useSelector((state) => state.boards);
+    const dispatch = useDispatch();
+    const [theme, setTheme] = useState(() => {
+      const storedTheme = localStorage.getItem("theme");
+      return storedTheme || "light";
+    });
+    const [sideBarOpen, setSideBarOpen] = useState(false);
+    const element = document.documentElement;
+  
+    useEffect(() => {
+      switch (theme) {
+        case "dark":
+          element.classList.add("dark");
+          localStorage.setItem("theme", "dark");
+          break;
+        case "light":
+          element.classList.remove("dark");
+          localStorage.removeItem("theme");
+          break;
+        default:
+          break;
+      }
+    }, [theme]);
+  
+    const selectedBoardId = boards.activeBoardId
+      ? boards.activeBoardId
+      : boards?.list[0].boardId;
+  
+    if (!boards.activeBoardId) {
+      dispatch(setActiveBoard({ boardId: selectedBoardId }));
+    }
+  
+    const selectedBoard = boards.list.find(
+      (board) => board.boardId === selectedBoardId
+    );
+    const selectedBoardName = selectedBoard ? selectedBoard.name : "";
+  
+    // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+     // console.log(uuidv4());
+    return (
+      <div className="h-screen grid grid-col-1 sm:grid-cols-[261px_1fr] grid-rows-[80px_1fr]">
+        <Navbar
+          selectedBoardName={selectedBoardName}
+          selectedBoardId={selectedBoardId}
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+        />
+        <SideBar theme={theme} setTheme={setTheme} sideBarOpen={sideBarOpen} />
+        <Board sideBarOpen={sideBarOpen} />
+      </div>
+    );
+  };
+  
+  export default App;*/
